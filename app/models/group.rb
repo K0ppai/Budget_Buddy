69c5cd2bb5fee28481b7445e15880a8347f39f62
K0ppai <psthu.koppai@gmail.com>
2023-10-16 18:15:21 +0630

    Add associations in models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :transfers, through: :transfer_groups
+  has_many :transfer_groups
   has_one_attached :icon
 end
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,3 +1,6 @@
 class Transfer < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :groups, through: :transfer_groups
+  has_many :transfer_groups
 end
+
