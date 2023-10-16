387ba32d22160a4bf78b15bee1ae05d3fc544f84
K0ppai <psthu.koppai@gmail.com>
2023-10-16 21:48:11 +0630

    Fix association error in models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-  has_many :transfers, through: :transfer_groups
   has_many :transfer_groups
+  has_many :transfers, through: :transfer_groups
   has_one_attached :icon
 end
